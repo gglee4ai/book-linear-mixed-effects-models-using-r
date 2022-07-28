24755e16b5a740153343f85f6652f3bab8fdc467
Gyeong-Geun LEE <gglee4ai@gmail.com>
2022-07-28 12:17:14 +0900

    .nb.html -> .html로 파일 이름 변경

--- tidy-note-files.R
+++ tidy-note-files.R
@@ -5,4 +5,8 @@ styler::style_dir()
 files <- list.files(pattern = "[.]Rmd$")
 for (f in files) try(rmarkdown::render(f))
 
-# 파일 이름중 nb.html을 .html로 바꿀 것
+# 파일 이름중 nb.html을 .html로 바꿀 것 github 업로드용
+notes <- list.files(pattern = "[.]nb.html$")
+renamed <- stringr::str_replace(notes, pattern = "[.]nb.html$", ".html")
+cat(str_c(notes, renamed, sep = " -> "), sep = "\n") # for check
+#file.rename(notes, renamed)
