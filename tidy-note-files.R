b4fdec5e698027a335e2c7964338f02b7f720a4e
Gyeong-Geun LEE <gglee4ai@gmail.com>
2022-07-29 22:51:45 +0900

    stringr 대신 base R로 명렁어 바꿈

--- tidy-note-files.R
+++ tidy-note-files.R
@@ -8,5 +8,5 @@ for (f in files) try(rmarkdown::render(f))
 # 파일 이름중 nb.html을 .html로 바꿀 것 github 업로드용
 notes <- list.files(pattern = "[.]nb.html$")
 renamed <- stringr::str_replace(notes, pattern = "[.]nb.html$", ".html")
-cat(stringr::str_c(notes, renamed, sep = " -> "), sep = "\n") # for check
+cat(paste(notes, renamed, sep = " -> "), sep = "\n") # for check
 # file.rename(notes, renamed)
